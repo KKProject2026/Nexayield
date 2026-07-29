b5050b63d50b4c6a5e3c017180e9ff851effbc91
KKProject2026 <townsearn@gmail.com>
2026-07-29 21:02:12 +0530

    Fix settings model to save company_qr column

--- NexaYield/backend/models/settings.js
+++ NexaYield/backend/models/settings.js
@@ -1,6 +1,7 @@
 module.exports = (sequelize, DataTypes) => {
   const Setting = sequelize.define('SETTINGS', {
-    company_wallet: { type: DataTypes.STRING }
+    company_wallet: { type: DataTypes.STRING },
+    company_qr: { type: DataTypes.STRING }
   }, {
     timestamps: false,
     tableName: 'SETTINGS'
